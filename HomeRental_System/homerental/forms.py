32cb2fef9c374b0809f7857ab492fe1047761f36
Md. Mohibur Rahman Nabil <67791679+mohiburnabil@users.noreply.github.com>
2023-05-31 20:51:49 +0600

    Update forms.py
    Forms for the functions

--- HomeRental_System/homerental/forms.py
+++ HomeRental_System/homerental/forms.py
@@ -20,3 +20,87 @@ class UserRegisterForm(UserCreationForm):
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
+ class UserUpdateForm(ModelForm):
+    class Meta:
+        model = models.Profile
+        fields = ['name', 'profile_image', 'email', 'phone', 'show_phone_number', 'location', 'social_facebook']
+
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+class HomeAddForm(ModelForm):
+    class Meta:
+        model = models.HomeAdd
+        fields = ['block_name', 'road_num', 'house_num', 'house_details','available_from', 'thumbnil', 'rent_ammount', 'house_type']
+        widgets = {'available_from':DateInput()}
+    def __init__(self, *args, **kwargs):
+        super(HomeAddForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+
+class ImageForm(ModelForm):
+    class Meta:
+        model = models.HomeImages
+        fields = ['image']
+
+    def __init__(self, *args, **kwargs):
+        super(ImageForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class NotificationForm(ModelForm):
+    class Meta:
+        model = models.AddNoticifation
+        fields = ['block_name', 'road_number', 'maximum_rent_ammount']
+
+    def __init__(self, *args, **kwargs):
+        super(NotificationForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class MassegeForm(ModelForm):
+    class Meta:
+        model = models.Message
+        fields = ['body']
+
+    def __init__(self, *args, **kwargs):
+        super(MassegeForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class Notification_settings_Form(ModelForm):
+    class Meta:
+        model = models.Profile
+        fields = ['get_notified']
+
+    def __init__(self, *args, **kwargs):
+        super(Notification_settings_Form, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class ComplainForm(ModelForm):
+    class Meta:
+        model = models.Complain
+        fields = ['complain']
+
+    def __init__(self, *args, **kwargs):
+        super(ComplainForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
